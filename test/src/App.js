a72bca0ecf4d70ec08b0fc6de23428927350d3b8
SarikaDonga <106614536+SarikaDonga@users.noreply.github.com>
2024-11-28 17:50:46 +0530

    Add files via upload

--- test/src/App.js
+++ test/src/App.js
@@ -9,38 +9,48 @@ import Setting from './Pages/Setting'
 import Dashbord from './Pages/Dashbord';
 import Navbar from './Components/Navbar'
 
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Profile1 from './Pages/Profile1';
 import SingleProductPage from './Pages/SingleProductPage';
+import Product from './Pages/Product';
+import ViewCart from './Pages/ViewCart';
+import AddProductPage from './Pages/AddProductPage';
+import { useState } from 'react';
 function App() {
+
+
   return (
     <>
-    <Router>
-      <Navbar/>
+      <Router>
+        <Navbar  />
 
-      <Routes>
+        <Routes>
 
-        <Route path='/' element={<Home/>}></Route>
-        <Route path="/product/:id" element={<SingleProductPage />} />
+          <Route path='/' element={<Home />}></Route>
+          <Route path="/product/:id" element={<SingleProductPage />} />
+          <Route path="/products" element={<Product/>} />
+          <Route path="/viewcart" element={<ViewCart  />} />
+          <Route path="/add-product" element={<AddProductPage />} />
 
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/profile1' element={<Profile1/>}></Route>
-        <Route path='/dashbord' element={<Dashbord/>}>
-          <Route path='profile' element={<Profile/>}></Route>
-          <Route path='setting' element={<Setting/>}></Route>
-        </Route>
-
-        
-
-      </Routes>
+         
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/profile1' element={<Profile1 />}></Route>
+          <Route path='/dashbord' element={<Dashbord />}>
+            <Route path='profile' element={<Profile />}></Route>
+            <Route path='setting' element={<Setting />}></Route>
+          </Route>
 
 
-    
-    </Router>
-    
-    
+
+        </Routes>
+
+
+
+      </Router>
+
+
     </>
   );
 }
